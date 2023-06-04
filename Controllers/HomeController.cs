63598e6770d357e3f95e810040900ad0f1855928
Gleidson <gleidson.moura@hotmail.com>
2023-06-04 17:43:14 -0300

    segundo commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
 using mvc1.Models;
+using System.Diagnostics;
 
 namespace mvc1.Controllers
 {
     public class HomeController : Controller
     {
-        private IRepository repository;
-        private string message;
-        public HomeController(IRepository repo, IConfiguration config)
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            repository = repo;
-            message = $"Docker - ({config["HOSTNAME"]})";
+            _logger = logger;
         }
+
         public IActionResult Index()
         {
-            ViewBag.Message = message;
-            return View(repository.Produtos);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
